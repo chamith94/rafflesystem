2fa3749d5df1318fa76ba257d83f725107944236
chamith94 <chamith.chathuka@gmail.com>
2016-02-04 01:19:53 +0530

    Removed unwanted content

--- dbScript.sql
+++ dbScript.sql
@@ -1,17 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.46-0ubuntu0.14.04.2 - (Ubuntu)
--- Server OS:                    debian-linux-gnu
--- HeidiSQL Version:             9.3.0.5046
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 -- Dumping database structure for raffle
 DROP DATABASE IF EXISTS `raffle`;
 CREATE DATABASE IF NOT EXISTS `raffle` /*!40100 DEFAULT CHARACTER SET latin1 */;
